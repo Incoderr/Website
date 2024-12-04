ed00862acdb284a9e57154000965ad71f0e1c857
Incoderr <laenartem75@gmail.com>
2024-12-04 20:53:38 +0500

    new

---
+++ SiteRe/src/Player-page.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import './Player-page.css';
+
+
+function PlayerPage() {
+// Состояния для иконок
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [isTracked, setIsTracked] = useState(false);
+// Обработчики кликов
+  const toggleBookmark = () => {
+    setIsBookmarked((prev) => !prev);
+};
+  const toggleTrack = () => {
+    setIsTracked((prev) => !prev);
+};
+
+
+//Плеер
+    const src = "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
+  
+ return (  
+  <div>
+    <header>
+        <div className="logo">
+            <i className="bi bi-camera-reels-fill fs-2"></i>
+            <h2>Label</h2>
+        </div>
+        <div className="search-box">
+            <input type="text" name="Поиск" id="" placeholder="Поиск"/>
+        </div>
+        <div className="nav-bar">
+            <nav className="nav-box">
+                <ul>
+                    <li>Главная</li>
+                    <li>Категории</li>
+                    <li>Профиль</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div className="container">
+            <div className="box-content">
+                <div className="info">
+                    <div className="image-info">
+                        <img src="55.jpg" alt="Poster"/>
+                        <button className="bookmark" onClick={toggleBookmark}>
+                            <i className={`bi ${isBookmarked ? "bi-bookmark-fill fs-5" : "bi-bookmark fs-5  "}`} />Буду смотреть</button>
+                        <button className="bookmark track" onClick={toggleTrack}>
+                            <i className={`bi ${isTracked ? "bi-bell-fill fs-5" : "bi-bell fs-5"}`} />Отслеживать</button>
+                    </div>
+                    <div className="more-info">
+                        <h2>Название</h2>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, optio accusamus est fugit sit quia labore quod rem, modi autem delectus eaque natus a perferendis ipsum doloremque vel voluptatibus beatae?
+                        </p>
+                        <h2 className="inf">Год выпуска:<a href="">2020</a></h2>
+                        <h2 className="inf">Жанр:<a href="">Комедия,Хоррор,Детектив</a></h2>
+                    </div>
+                </div>
+                <div className="video-box">
+                   <div className="player">
+                        <VideoPlayer src={src} autoPlay={false} />
+                   </div>
+                </div>
+                <div className="test">
+
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+  </div>
+  );
+}
+
+export default PlayerPage;
---
+++ SiteRe/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Ваш основной файл JS
+  output: {
+    filename: 'bundle.js', // Название выходного файла
+    path: path.resolve(__dirname, './dist'), // Папка для выходных файлов
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,  // Обработка файлов .js и .jsx
+        exclude: /node_modules/, // Правильное исключение node_modules
+        use: {
+          loader: 'babel-loader', // Используем babel-loader для трансформации JSX
+        },
+      },
+      {
+        test: /\.css$/,  // Обработка CSS
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Поддержка .js и .jsx файлов
+  },
+  mode: 'development', // Режим разработки
+  devServer: {
+    historyApiFallback: true,
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    open: true,
+    setupMiddlewares: (middlewares, devServer) => {
+      devServer.app.use((req, res, next) => {
+        console.log(`Request URL: ${req.url}`);
+        next();
+      });
+      return middlewares;
+    }
+  },
+};
